c108d72b3f084711a0936bc04d9386535726eed3
Fernando Almeida <58122934+Tooht@users.noreply.github.com>
2026-04-12 21:58:43 +0100

    Delete 0.0.1 directory

--- 0.0.1/Functions/Public/Set-SQLPackConfig.ps1
+++
@@ -1,6 +0,0 @@
-function Set-SQLPackConfig {
-    param([hashtable]$Config)
-
-    $configFile = Join-Path $PSScriptRoot "..\Config\settings.json"
-    $Config | ConvertTo-Json -Depth 5 | Set-Content $configFile
-}
--- 0.0.1/Tests/Get-DbaHealth.Tests.ps1
+++
@@ -1,8 +0,0 @@
-Describe "Get-DbaHealth" {
-    It "Deve devolver um objeto com propriedades essenciais" {
-        $result = Get-DbaHealth -Instance "localhost"
-
-        $result.Instance | Should -Not -BeNullOrEmpty
-        $result.Version  | Should -Not -BeNullOrEmpty
-    }
-}
